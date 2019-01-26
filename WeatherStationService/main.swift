06e39ad3a3b90e77db736bd34f33c7782bfd8fde
Fabio Gallonetto <fabio.gallonetto@schibsted.com>
2019-01-26 09:10:27 +0000

    Initial Commit

---
+++ WeatherStationService/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WeatherStationService
+//
+//  Created by Fabio Gallonetto on 26/01/2019.
+//  Copyright © 2019 Fabio Gallonetto. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
